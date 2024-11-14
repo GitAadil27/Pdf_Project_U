d570328de2005ab0b643f8ee30105e8d92674988
GitAadil27 <cityadil024@gmail.com>
2024-11-14 14:41:08 +0300

    Update settings.py

--- PDF_Project/settings.py
+++ PDF_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s9tlp$709+8eu%m6b0v3dt@4q@tku$*!+10f#n=))(3mz(78*q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'https://cbkupload.onrender.com','cbkupload.onrender.com/']
+ALLOWED_HOSTS = ['*', 'https://cbkupload.onrender.com','cbkupload.onrender.com']
 
 
 # Application definition
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pdf_project.wsgi.application'
 CSRF_TRUSTED_ORIGINS = ['*', 'https://cbkupload.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['*', 'cbkupload.onrender.com']
 
 
 # Database
