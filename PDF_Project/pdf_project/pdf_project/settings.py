2fe1f81a4363cd38fe990405a93ee2373bfce67a
GitAadil27 <cityadil024@gmail.com>
2024-11-14 18:44:13 +0300

    Update settings.py

--- PDF_Project/pdf_project/pdf_project/settings.py
+++ PDF_Project/pdf_project/pdf_project/settings.py
@@ -32,7 +32,10 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://cbkupload.onrender.com',
+    'cbkupload.onrender.com'
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
